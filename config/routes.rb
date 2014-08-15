23dc18cc154a025cd12f1c61981c1eabfc418df9
Jonathan Lee <jonathanbarlowlee@gmail.com>
2014-08-14 17:37:15 -0700

    Add root '/' on a whim.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ FirstApp6::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root '/'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
